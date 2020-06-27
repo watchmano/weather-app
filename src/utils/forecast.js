d6d80179a61234989748fe50d4116d7eafcc593d
Mark <mark@Markui-MacBookAir.local>
2020-06-27 23:52:19 +0900

    Setup app for Heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const request = require('postman-request')
 
 const forecast  = (a, b, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=5dc7c4cb950e9b8942489b407a3d5212&units=s&query=" + a + "," + b + "&units=f"
+    const url = "http://api.weatherstack.com/current?access_key=5dc7c4cb950e9b8942489b407a3d5212&units=s&query=" + a + "," + b + "&units=m"
     
     //key 값 이름 바꾸지 말자. url 외의 다른 이름 넣어주면 작동 안됨.
     request({url, json: true}, (error, {body}) => {
